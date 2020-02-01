2b778f7186ae300e19f071c4e43952df11c04496
bgotchall <bgotchall@gmail.com>
2020-02-01 11:10:43 -0600

    added pics and switched to local directories

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
  (function() {
-    Galleria.loadTheme('https://cdnjs.cloudflare.com/ajax/libs/galleria/1.5.7/themes/classic/galleria.classic.min.js');
+    //Galleria.loadTheme('https://cdnjs.cloudflare.com/ajax/libs/galleria/1.5.7/themes/classic/galleria.classic.min.js');
+    Galleria.loadTheme("./galleria/dist/themes/folio/galleria.folio.min.js");   //local
     Galleria.run('.galleria');
 }());
